f1b6c5b8ad8eefc069427694331d1b7aa03e4461
riplolab <aktobuf2@gmail.com>
2025-08-16 13:36:21 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+// lifemeal PWA service worker
+const APP_CACHE = "lifemeal-app-v1";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./sw.js",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(APP_CACHE).then((c) => c.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.map((k) => k !== APP_CACHE ? caches.delete(k) : null)))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  const url = new URL(e.request.url);
+  const isData = url.hostname.includes("docs.google.com") || url.pathname.endsWith(".csv");
+  if (isData) {
+    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+    return;
+  }
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+});
