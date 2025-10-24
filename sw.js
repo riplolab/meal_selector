b9008c9b673c27500b3d2d4c5ebd40c76b31351c
CMS <226812232+riplolab@users.noreply.github.com>
2025-10-24 20:09:56 +0900

    Align PWA icon paths with actual files

--- sw.js
+++ sw.js
@@ -5,8 +5,8 @@ const APP_SHELL = [
   "./index.html",
   "./manifest.webmanifest",
   "./sw.js",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
 self.addEventListener("install", (e) => {
